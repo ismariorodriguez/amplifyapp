6d2ee5eb347295299e0a28dfab5cb5bec159d1f5
Mario Rodriguez <ismariorodriguez@yahoo.com>
2021-10-19 21:13:58 -0400

    feature 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test
+          Test (MRod 123)
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
