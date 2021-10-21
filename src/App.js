cde31527f2a1d39a5be163dff26cd1add01ebbfe
Mario Rodriguez <ismariorodriguez@yahoo.com>
2021-10-21 17:32:47 -0400

    change one

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test (MRod 123)
+          Test (MRod 123 456)
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
