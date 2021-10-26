7ca41a4a2782fbcf50cda2500382ba21aeb10cc2
Mario Rodriguez <ismariorodriguez@yahoo.com>
2021-10-26 11:09:33 -0400

    sprint 2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Test (MRod 123 456)
+          Test One
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
