6cfc323ded28574f44c2a8c22ec4982c095fbb95
Mario Rodriguez <ismariorodriguez@yahoo.com>
2021-10-28 18:23:14 -0400

    label change MRod

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
+      <h1>MRod curde app</h1>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
         placeholder="Note name def"
